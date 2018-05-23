1ec362f18c143dfff4e7f8acf238c1bab0ad147a
yujmo <yujmo@users.noreply.github.com>
2018-05-23 12:25:14 +0800

    Update ps_start.sh

--- ps_start.sh
+++ ps_start.sh
@@ -3,4 +3,4 @@ apt-get install git -y
 git clone https://github.com/tensorflow/benchmarks.git
 python tf_cnn_benchmarks.py --local_parameter_device=gpu --batch_size=64 --model=resnet50 --variable_update=distributed_replicated \
 --job_name=ps --ps_hosts=ps-1:50000 \
---worker_hosts=worker-1:50001,10.0.0.2:50001 --task_index=0
+--worker_hosts=worker-1:50001,worker-2:50001 --task_index=0
